4abd314879275150d1b3c62f7d186f75ca4a9319
MichelleCho <mcshooter@gmail.com>
2014-10-18 11:37:16 -0700

    location file update

--- location.js
+++ location.js
@@ -2,7 +2,7 @@ var initialLocation;
 
 function initialize() {
   var myOptions = {
-    zoom: 20,
+    zoom: 17,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
@@ -13,7 +13,11 @@ function initialize() {
     navigator.geolocation.getCurrentPosition(function(position) {
       initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
       map.setCenter(initialLocation);
-      
+      var marker = new google.maps.Marker({
+        position: initialLocation,  
+        map: map,
+        
+      });  
     }, function() {
       handleNoGeolocation(browserSupportFlag);
     });
